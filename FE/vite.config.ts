14d731ba3a64d37a6fa62b02add1cee860213d36
tan213123 <htan3522-tech@users.noreply.github.com>
2026-06-28 15:06:10 +0700

    port FE

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 const apiProxyTarget =
-  "https://1ceb-2001-ee0-4f0d-98b0-d1-682a-c996-692e.ngrok-free.app";
+  "https://dbdd-2001-ee0-4f0d-98b0-d1-682a-c996-692e.ngrok-free.app";
 
 // Proxy để call API Backend
 export default defineConfig({
