a58b7bca34a531ea637071becddd93e7bc511fca
tan213123 <htan3522-tech@users.noreply.github.com>
2026-06-28 17:38:02 +0700

    Add the file of vegetarian restaurants

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 const apiProxyTarget =
-  "https://dbdd-2001-ee0-4f0d-98b0-d1-682a-c996-692e.ngrok-free.app";
+  //"https://dbdd-2001-ee0-4f0d-98b0-d1-682a-c996-692e.ngrok-free.app";
+  "http://localhost:8080";
 
 // Proxy để call API Backend
 export default defineConfig({
