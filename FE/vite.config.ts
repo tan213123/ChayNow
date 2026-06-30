f7f0e78bbba54201f9ea70464ab899a2fdef0f3e
huynhngocduy <huynhngocduy3122021@gmail.com>
2026-06-30 08:21:39 +0700

    fix: Update API proxy target and server port configuration

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const apiProxyTarget =
-  "https://dbdd-2001-ee0-4f0d-98b0-d1-682a-c996-692e.ngrok-free.app";
+const apiProxyTarget = "http://localhost:8081";
 
 // Proxy để call API Backend
 export default defineConfig({
